859633370aae1369408bae3b2654aa9bc6a27388
Gregory_Riley <gregr769@hsd.k12.or.us>
2024-02-21 10:27:17 -0800

    title

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{   
+    public GameObject ControlMenuPanel;
+
+    void Start()
+    {
+        ControlMenuPanel.SetActive(false);
+    }
+
+    public void OnControls()
+    {
+        ControlMenuPanel.SetActive(true);
+    }
+
+    public void OnBack()
+    {
+        ControlMenuPanel.SetActive(false);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
